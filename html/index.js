492fd32681ce6cec8df093ae10a26c733a28205e
Thomas <tbe@pm.me>
2023-09-18 12:38:14 +0200

    moved checksum and displays in new funtion

--- html/index.js
+++ html/index.js
@@ -2,13 +2,21 @@
 // consts
 const req = new XMLHttpRequest;
 let res = new Object;
-
+let hexOutputArray = [];
 //selectors
 const table = document.querySelector('.main_table');
+const hexoutputFinal = document.querySelector('.result h1');
+const title = document.querySelector('.command_name p');
+const desc = document.querySelector('.command_description p');
+const moreinfo = document.querySelector('.command_moreinfo');
+const selects = document.querySelector('#options');
+const selectsFull = document.querySelector('.selects');
+const input_id = document.querySelector('.id_input');
+const openInput = document.querySelector('.open_input');
+const dInputs = document.querySelectorAll('.d');
 
 //eventlisteners
 table.addEventListener('click', selectItem )
-
 
 function getData() {
     req.open('get','../data/mdc.commands.json')
@@ -19,7 +27,6 @@ function getData() {
     }
     req.send();
 }
-
 function createTable(e){ 
 
     const newTr = document.createElement('tr');
@@ -46,7 +53,6 @@ function createTable(e){
 
     table.appendChild(newTr);
 }
-
 function selectItem(e) {
     let comm ="";
     comm = e.target.parentElement.childNodes[1].innerText;
@@ -56,40 +62,94 @@ function selectItem(e) {
     console.log( "currentData:", currentData);
     showDetails(currentData);
 }
-
 function showDetails(data) {
-    // console.log(data)
-    //Selectors
-    const title = document.querySelector('.command_name p')
-    const desc = document.querySelector('.command_description p')
-    const input_id = document.querySelector('.id_input')
-    const moreinfo = document.querySelector('.command_moreinfo')
-    const selects = document.querySelector('.selects')
-    
+    //get all needed data
     let hasFixedValues = data[0].hasfixedvalues || false;
     let hasSubCmd = data[0].hassubcmd || false;
-let dataLength = data[0].datalength;
-let image = data[0].image;
-let command = data[0].command;
-let values = data[0].values || [] ;
-let moreInfo = data[0].moreinfo || "";
+    let subCmd = data[0].subcmd || "";
+    let dataLength = data[0].datalength === "variable" ? null : data[0].datalength;
+    let image = data[0].image;
+    let command = data[0].command;
+    let values = data[0].values || [] ;
+    let moreInfo = data[0].moreinfo || "";
+    let outData = [];
 
-console.log( )
+    //showing or hidding parts based on selectors:
+        //hide all d inout fields as long as they arent needed
+        document.querySelectorAll('.d-div').forEach(e => e.classList.add('hidden'));
+        //show Fixed Value Selector and refresh Options
+        hasFixedValues ? (createOptionsSelect(values), selectsFull.classList.remove('hidden')): selectsFull.classList.add('hidden')
+        //show or remove the more info box
+        moreInfo ? moreinfo.classList.remove('hidden') : moreinfo.classList.add('hidden')
+        //show inputField for non fixed values
+        let neededFiels = dataLength -1;   //create if to decide when to remove 1 or 2 based on hasSubCmd
+        if (neededFiels > 0) {
+            for (let i=1; i <= neededFiels; i++) {
+                document.querySelector('.d'+i).classList.remove('hidden')
+            }
+        }
 
-hasFixedValues ? (createOptionsSelect(values), selects.classList.remove('hidden')): selects.classList.add('hidden')
-moreInfo ? moreinfo.classList.remove('hidden') : moreinfo.classList.add('hidden')
+        // show
+    
+    // show already known Data:
+    title.innerText = `Function: ${data[0].name}` ;
+    desc.innerText = data[0].Description;
+    moreinfo.innerText = moreInfo;
+    
+    //outData Format AA, command, id, Datalenght, (subcmd), D1 -N, Checksum
 
-// Definers
-title.innerText = data[0].name;
-desc.innerText = data[0].Description;
-data[0].moreinfo != null ? moreinfo.innerText = data[0].moreinfo : moreinfo.innerText = "";
-// console.log(data)
+    calculate();
+    //Eventlisteners (fixed values, no subCmd)
+    selects.addEventListener('input', calculate);
+    input_id.addEventListener('input', calculate);
+    openInput.addEventListener('input', calculate);
+
+    function calculate(){
+        if(hasFixedValues && !hasSubCmd && dataLength) {
+            // calculate if openInput fiels are needes (-1 because this is from the selector)
+            outData = [command, input_id.value,dataLength,selects.value]
+
+            if (neededFiels == 0) showHEX(outData)
+            if (neededFiels > 0) {
+
+                for (let i=1; i<=neededFiels; i++) { outData.push(document.querySelector('#d'+i).value) }
+                showHEX(outData);
+            }
+        }
+
+        if(hasFixedValues && hasSubCmd && dataLength) {
+            console.log("Case2: fixed values AND subCmd")
+
+            outData = [command, input_id.value,dataLength, subCmd ,selects.value]
+            
+            showHEX(outData)
+            console.log(dataLength)
+
+        }
+
+        if(!hasFixedValues && !hasSubCmd)  {
+            console.log("Case3: NO fixed values AND NO subCmd")
+
+            console.log(dataLength)
+
+
+        }
+
+
+        if(!hasFixedValues && hasSubCmd) {
+            console.log("Case4: NO fixed values but has subCmd")
+            console.log(dataLength)
+
+        }
+    };
+    
 };
 
 
 function createOptionsSelect(values){
     
-    const val = document.querySelector('#values')
+    const val = document.querySelector('#options')
+    removeAllChildNodes(val);
     
     let res =  values.forEach( e => {
         const newOption = document.createElement('option')  
@@ -97,7 +157,28 @@ function createOptionsSelect(values){
         newOption.innerText = e.name
         val.appendChild(newOption)
     })
-// return res
+    // return res
+}
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+
+
+function showHEX(val) {
+    
+    //output Resule
+    let totalForChecksumDec = 0;
+    val.forEach( e => {totalForChecksumDec += parseInt(e,16)})
+
+    let checksum = Math.abs(totalForChecksumDec).toString(16).toUpperCase().slice(-2);
+    
+    val.push(checksum);
+    val.unshift('AA');
+    let outputresult = val.join(' ');
+    hexoutputFinal.innerText = outputresult;
+    
 }
 
 getData();
