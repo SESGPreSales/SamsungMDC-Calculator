a7dfaafd61d6a3056693ac2452d67a3163bf5b12
unknown <tbe@pm.me>
2026-04-19 13:49:00 +0200

    corrected DB (git Issue 11)

--- db/init/import-script.sh
+++ db/init/import-script.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-
 # Sleep for few seconds to make sure MongoDB has started
 sleep 10
-
 # Import data into MongoDB
 mongoimport --host localhost --db mdc --collection commands --file /mdc.commands.json --jsonArray
 sleep 2 
